57c7f8e6f918c7cb6012b06691052b52f1c4ce09
supreeth101 <43764745+supreeth101@users.noreply.github.com>
2020-08-25 22:02:12 -0500

    Add files via upload

---
+++ precache-manifest.a552ad69779d898270471c424f745a94.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4353b201c327208e01da29bc5a5bba5",
+    "url": "/remote-roofing-challenge-app/index.html"
+  },
+  {
+    "revision": "f79f52c832efcd3886e8",
+    "url": "/remote-roofing-challenge-app/static/css/main.e13d94e5.chunk.css"
+  },
+  {
+    "revision": "23c4d7ea50c6a56035e1",
+    "url": "/remote-roofing-challenge-app/static/js/2.10015176.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/remote-roofing-challenge-app/static/js/2.10015176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79f52c832efcd3886e8",
+    "url": "/remote-roofing-challenge-app/static/js/main.1a5087b7.chunk.js"
+  },
+  {
+    "revision": "922852d41b0c5e0443e7",
+    "url": "/remote-roofing-challenge-app/static/js/runtime-main.87e27bd3.js"
+  },
+  {
+    "revision": "3b3d05376646ecf077f4e1b4550fc56f",
+    "url": "/remote-roofing-challenge-app/static/media/background.3b3d0537.jpg"
+  },
+  {
+    "revision": "e417ebcb8eeaedf98178c7b70e212225",
+    "url": "/remote-roofing-challenge-app/static/media/logo.e417ebcb.png"
+  },
+  {
+    "revision": "3bd3b8462c621ce54462f1efc201c085",
+    "url": "/remote-roofing-challenge-app/static/media/pin.3bd3b846.svg"
+  }
+]);
